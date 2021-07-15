fa4dafc8949346c0c9b86809d91dcb0beee84dc1
Diego Rufino <diegorufino1@gmail.com>
2021-07-14 23:30:36 -0300

    Criacao dos temas de cores da aplicacao - dark e light

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import GlobalSyles from './styles/GlobalSyles';
 
 import Layout from './components/Layout';
+import dark from './styles/themes/dark';
 
 const App: React.FC = () => {
     return (
-        <>
+        <ThemeProvider theme={dark}>
             <GlobalSyles />
             <Layout />
-        </>
+        </ThemeProvider>
     )
 }
 
