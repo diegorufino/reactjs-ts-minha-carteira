20e7427fd9c0cc51722ee746c178655e01c6c19a
Diego Rufino <diegorufino1@gmail.com>
2021-07-19 22:47:34 -0300

    Content Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from 'styled-components';
 import GlobalSyles from './styles/GlobalSyles';
 
 import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
 import dark from './styles/themes/dark';
 
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
             <GlobalSyles />
-            <Layout />
+            <Layout>
+                <Dashboard />
+            </Layout>
         </ThemeProvider>
     )
 }
