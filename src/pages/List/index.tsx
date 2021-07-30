5289d878d52318532d8ddc087ba02627b0b0e428
Diego Rufino <diegorufino1@gmail.com>
2021-07-29 22:42:48 -0300

    Formatando valor e data

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -6,6 +6,8 @@ import HistoryFinanceCard from '../../components/HistoryFinanceCard'
 
 import gains from '../../repositories/gains'
 import expenses from '../../repositories/expenses'
+import formatCurrency from '../../utils/formatCurrency'
+import formatDate from '../../utils/formatDate'
 
 import { Container, Content, Filters } from './styles';
 
@@ -62,9 +64,9 @@ const List: React.FC<IRoutesParams> = ({ match }) => {
             return {
                 id: String(Math.random () * data.length),
                 description: item.description,
-                amount: item.amount,
+                amount: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
-                dataFormatted: item.date,
+                dataFormatted: formatDate(item.date),
                 tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
             }
         } )
